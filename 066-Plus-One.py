6140d2d5c1da6357b2296603a10e009bee05b97f
QuenLo <quenlo510@gmail.com>
2018-05-01 15:47:38 +0800

    Update 066-Plus-One.py
    update v2 code

--- 066-Plus-One.py
+++ 066-Plus-One.py
@@ -1,24 +1,18 @@
 import math
 
 class Solution(object):
-    def plusOne(self, digits):
+    def plusOne(self,digits):
 
         AnsInt = 0
         Answer = []
-        # switch to int
-        for digit in digits:
-            AnsInt = int(digit) + 10*AnsInt
         
-        # plus one to int
-        AnsInt += 1
-        
-        # switch back to List
-        while AnsInt > 0 :
-            Append_Temp = AnsInt % 10
-            Answer.append(Append_Temp)
-            AnsInt = AnsInt/10
-        
-        # The answer
-        Answer.reverse()
-        return Answer
-        
+        if digits[-1] < 9 :
+            if len(digits) > 1:
+                return digits[:-1] + [digits[-1]+1]
+            else :
+                return [digits[-1]+1]
+        else :
+            if len(digits) > 1:
+                return plusOne(digits[:-1]) + [0]
+            else :
+                return [1,0]
