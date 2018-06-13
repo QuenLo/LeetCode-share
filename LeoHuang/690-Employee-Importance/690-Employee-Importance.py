bf79c38b37929046d972aaffdf5f3762723489f6
Huang, Li-Pang <leo89214749@hotmail.com>
2018-06-13 14:08:06 +0800

    improve 690 to O(n)

--- LeoHuang/690-Employee-Importance/690-Employee-Importance.py
+++ LeoHuang/690-Employee-Importance/690-Employee-Importance.py
@@ -19,12 +19,12 @@ class Solution(object):
     """
     totalImportanceValue = 0
 		# sub_ids stores subordinates' ids
-    sub_ids = [id]
+		sub_ids = [id]
+    idToEmployeesIndex = {}
+    for i in range(len(employees)):
+      idToEmployeesIndex[employees[i].id] = i
     while len(sub_ids) != 0:
       now_id = sub_ids.pop(0)
-      i = 0
-      while now_id != employees[i].id:
-        i += 1
-      totalImportanceValue += employees[i].importance
-      sub_ids.extend(employees[i].subordinates)
+      totalImportanceValue += employees[idToEmployeesIndex[now_id]].importance
+      sub_ids.extend(employees[idToEmployeesIndex[now_id]].subordinates)
     return totalImportanceValue
