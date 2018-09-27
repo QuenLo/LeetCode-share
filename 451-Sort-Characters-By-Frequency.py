5c9c8027c5a15626f000a84e75c9f5d389ee6a1a
Quen Luo <quenlo510@gmail.com>
2018-09-27 21:40:23 +0800

    solution using Counter

--- 451-Sort-Characters-By-Frequency.py
+++ 451-Sort-Characters-By-Frequency.py
@@ -1,16 +1,9 @@
+from collections import Counter
 class Solution(object):
     def frequencySort(self, s):
         
-        letterDict = {}
-        AnswerStr = ""
-        for ss in s:
-            if letterDict.has_key(ss) :
-                letterDict[ss] += 1
-            else:
-                letterDict[ss] = 1
-                
-        #sorted(iterable, cmp=None, key=None, reverse=False)
-        letterTuple = sorted(letterDict.items(), key=lambda letter: letter[1],reverse=True) 
+        letterTuple = Counter(s).most_common()
+        AnswerStr = "" 
         
         for key in letterTuple:
             AnswerStr += key[0]*key[1]
