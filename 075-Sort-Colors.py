a89d4ec9086e9a3501b3e228b8f7c99fe25b3ec4
Quen Luo <quenlo510@gmail.com>
2018-11-18 23:13:49 +0800

    Update 075-Sort-Colors.py

--- 075-Sort-Colors.py
+++ 075-Sort-Colors.py
@@ -4,17 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
         """
+        index0, index1 = 0, 0
         
-        index0, index1, index2 = 0, 0, len(nums)-1
-        
-        while index1 <= index2:
-            if nums[index1] == 0:
-                nums[index0], nums[index1] = 0, nums[index0]
+        for i in range(0, len(nums)):
+            v = nums[i]
+            nums[i] = 2
+            
+            if v == 0:
+                nums[index1] = 1
                 index1 += 1
+                nums[index0] = 0
                 index0 += 1
-            elif nums[index1] == 2:
-                nums[index2], nums[index1] = 2, nums[index2]
-                #index1 += 1
-                index2 -= 1
-            else:
+            elif v == 1:
+                nums[index1] = 1
                 index1 += 1
