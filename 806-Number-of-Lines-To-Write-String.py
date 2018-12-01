12e6d20908fb93dae1c4ee99dfb87343e08e26cf
Quen Luo <quenlo510@gmail.com>
2018-12-02 00:08:55 +0800

    Update 806-Number-of-Lines-To-Write-String.py

--- 806-Number-of-Lines-To-Write-String.py
+++ 806-Number-of-Lines-To-Write-String.py
@@ -1,12 +1,13 @@
 class Solution(object):
     def numberOfLines(self, widths, S):
         total,raw = 0, 1
+        Ord_a = ord('a')
         for s in S:
-            total += widths[ord(s)-ord('a')]
-            if(total == 100):
-                total = 0
-                raw += 1
-            elif(total > 100):
-                total = widths[ord(s)-ord('a')]
+            total += widths[ord(s)-Ord_a]
+            if(total > 100):
+                total = widths[ord(s)-Ord_a]
                 raw +=1
+        if total == 100:
+            raw += 1
+            total = 0
         return [raw, total]
