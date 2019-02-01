f5ae51d6a8d3f3df82e564eb67ba265c2878f032
Quen Luo <quenlo510@gmail.com>
2019-02-01 19:08:08 +0800

    Update 068-Text-Justification.py

--- 068-Text-Justification.py
+++ 068-Text-Justification.py
@@ -1,43 +1,27 @@
 class Solution(object):
     def fullJustify(self, words, maxWidth):
-        words_len, current_string = len(words), ""
-        Ans_list, current_list, current_words = [], [], 0
-        i = 0
-        while i < words_len:
-            if len(current_string) + len(words[i]) + 1 > maxWidth:
-                if current_words == 1:
-                    current_string += " "*(maxWidth - len(current_string))
-                elif current_string == "":
-                    current_string = words[i]
-                    current_string += " "*(maxWidth - len(current_string))
-                    i += 1
-                else :
-                    rest_space = maxWidth - len(current_string)
-                    if not rest_space % (current_words - 1):
-                        split_space = int(rest_space / (current_words - 1)) + 1
-                        current_string = (" "*split_space).join(current_list)
-                    else:
-                        split_space = int( rest_space // (current_words - 1) ) + 1
-                        begin_space = rest_space % (current_words - 1)
-                        current_string = (" "*(split_space+1)).join(current_list[:begin_space+1])
-                        current_string += " "*(split_space)
-                        current_string += (" "*(split_space)).join(current_list[begin_space+1:])
+        
+        current_string = ""
+        Ans_list, current_list = [], []
 
-                Ans_list.append(current_string)    
+        for word in words:
+            if len(current_string) + len(word) + len(current_list) > maxWidth:
+                if len(current_list) == 1:
+                    Ans_list.append( current_list[0] + " "*(maxWidth - len(current_list[0])) )
+                else:
+                    rest_space = maxWidth - len(current_string)
+                    split_space, begin_space = divmod( rest_space, len(current_list) - 1 )
+                    current_string = (" "*(split_space+1)).join(current_list[:begin_space+1])
+                    current_string += " "*(split_space)
+                    current_string += (" "*(split_space)).join(current_list[begin_space+1:])
+                    Ans_list.append(current_string)
+                # clear all
                 current_string = ""
-                current_words = 0
                 del current_list[:]
-            elif i == words_len -1 :
-                current_list.append(words[i])
-                current_string = " ".join(current_list)
-                current_string += " "*(maxWidth - len(current_string))
-                Ans_list.append(current_string)
-                break
-            else:
-                current_list.append(words[i])
-                current_string = " ".join(current_list)
-                current_words += 1
-                i += 1
+            # add new word
+            current_list.append(word)
+            current_string += word
+
+        Ans_list.append( ' '.join(current_list) + ' '*(maxWidth - len(current_string) - len(current_list) + 1) )
         
         return Ans_list
-        
