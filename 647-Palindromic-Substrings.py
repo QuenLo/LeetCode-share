d6af74d8e064b437f4912dbb3175cff07fe933b7
Quen Luo <quenlo510@gmail.com>
2019-03-05 03:29:21 +0800

    add another solution

--- 647-Palindromic-Substrings.py
+++ 647-Palindromic-Substrings.py
@@ -24,3 +24,21 @@ class SolutionII:
                 if s[i:j+1] == s[i:j+1][::-1]:
                     count += 1
         return count
+
+# faster
+class SolutionIII:
+    def countSubstrings(self, s: str) -> int:
+        count, Length = 0, len(s)
+        
+        # there are 2*Length - 1 centers
+        # s[1], between s[1] and s[2], s[2], between s[2] and s[3] ... etc
+        for center in range( 2*Length - 1 ):
+            left = center // 2
+            right = left + center % 2
+            
+            while( left >= 0 and right < Length and s[left] == s[right] ):
+                count += 1
+                left -= 1
+                right += 1
+        
+        return count
