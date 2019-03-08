3129728f38c149cdcaf5ef50256a800dbe3f8782
Quen Luo <quenlo510@gmail.com>
2019-03-08 21:26:35 +0800

    complete problem 977

---
+++ 977-Squares-of-a-Sorted-Array.py
@@ -0,0 +1,33 @@
+# Space Complexity: O(N)
+# Time Complexity: O(N), where N is the length of A.
+
+class Solution:
+    def sortedSquares(self, A: List[int]) -> List[int]:
+        Ans = collections.deque()
+        r,l = len(A)-1, 0
+        while( l <= r ):
+            left, right = A[l]**2, A[r]**2
+            if left > right:
+                Ans.appendleft( left )
+                l += 1
+            else:
+                Ans.appendleft( right )
+                r -= 1
+        
+        return list(Ans)
+        
+# list
+class SolutionII:
+    def sortedSquares(self, A: List[int]) -> List[int]:
+        Ans = [0]*(len(A))
+        r,l = len(A)-1, 0
+        while( l <= r ):
+            left, right = A[l]**2, A[r]**2
+            if left > right:
+                Ans[r-l] = left
+                l += 1
+            else:
+                Ans[r-l] = right
+                r -= 1
+        
+        return Ans
