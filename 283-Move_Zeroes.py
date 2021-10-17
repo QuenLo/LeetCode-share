a5447b1544e5f33745963f865bfa16ef88159a12
Quen Luo <quenlo510@gmail.com>
2021-10-16 23:22:00 -0500

    Update 283-Move_Zeroes.py

--- 283-Move_Zeroes.py
+++ 283-Move_Zeroes.py
@@ -13,3 +13,16 @@ class Solution:
                 i += 1
                 
         nums.extend([0]*count)
+        
+class SolutionII:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+
+        i = 0
+        for cur in range(len(nums)):
+            if nums[cur]:
+                temp = nums[i], nums[cur]
+                nums[cur], nums[i] = temp
+                i += 1
