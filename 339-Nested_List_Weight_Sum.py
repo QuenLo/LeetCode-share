a2ec45dc0296a83a4c43f007e948d32df316566d
Quen Luo <quenlo510@gmail.com>
2021-10-27 19:36:46 -0500

    Create 339-Nested_List_Weight_Sum.py
    https://leetcode.com/problems/nested-list-weight-sum/

---
+++ 339-Nested_List_Weight_Sum.py
@@ -0,0 +1,62 @@
+# """
+# This is the interface that allows for creating nested lists.
+# You should not implement it, or speculate about its implementation
+# """
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
+#    def isInteger(self):
+#        """
+#        @return True if this NestedInteger holds a single integer, rather than a nested list.
+#        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
+#        """
+#
+#    def getInteger(self):
+#        """
+#        @return the single integer that this NestedInteger holds, if it holds a single integer
+#        Return None if this NestedInteger holds a nested list
+#        :rtype int
+#        """
+#
+#    def getList(self):
+#        """
+#        @return the nested list that this NestedInteger holds, if it holds a nested list
+#        Return None if this NestedInteger holds a single integer
+#        :rtype List[NestedInteger]
+#        """
+
+class Solution:
+    def depthSum(self, nestedList: List[NestedInteger]) -> int:
+        
+        def count( nestedL, total, depth ):
+            # a single interger
+            if nestedL.isInteger():
+                return total + nestedL.getInteger()*depth
+            
+            
+            for L in nestedL.getList():
+                total = count( L, total, depth+1 )
+            
+            return total
+        
+        ans = 0
+        for nested in nestedList:
+            ans += count( nested, 0, 1 )
+        
+        return ans
