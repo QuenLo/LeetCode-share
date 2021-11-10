d60e93c2640ea7e6aff502880e518b81a587ba64
Quen Luo <quenlo510@gmail.com>
2021-11-10 16:02:47 -0600

    Create 726-Number_of_Atoms.py
    https://leetcode.com/problems/number-of-atoms/

---
+++ 726-Number_of_Atoms.py
@@ -0,0 +1,32 @@
+class Solution:
+    def countOfAtoms(self, formula: str) -> str:
+        
+        N = len(formula)
+        stack = [collections.Counter()]
+        i = 0
+        
+        while i < N:
+            if formula[i] == '(':
+                stack.append( collections.Counter() )
+                i += 1
+                
+            elif formula[i] == ')':
+                top = stack.pop()
+                i += 1
+                i_start = i
+                while i < N and formula[i].isdigit(): i += 1
+                count = int( formula[i_start:i] or 1 )
+                for name, v in top.items():
+                    stack[-1][name] += v*count
+                    
+            else:
+                i_start = i
+                i += 1
+                while i < N and formula[i].islower(): i += 1
+                name = formula[i_start:i]
+                i_start = i
+                while i < N and formula[i].isdigit(): i+= 1
+                count = int( formula[i_start:i] or 1 )
+                stack[-1][name] += count
+                
+        return "".join(  name + (str(stack[-1][name]) if stack[-1][name] > 1 else '' )  for name in sorted(stack[-1]) )
